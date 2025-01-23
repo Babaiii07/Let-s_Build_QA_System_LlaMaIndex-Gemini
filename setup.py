26366e5d2cdb59ef274fd397e6362172022fcfd1
parthib karak <parthibkarak2004@gmail.com>
2025-01-23 17:42:19 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Parthib Karak',
+    author_email= 'parthibkarak2004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
